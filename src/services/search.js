37f504ff16036f1a6074348a06c55f3f7bef5b29
moon04 <sydeny254@gmail.com>
2022-01-15 00:16:08 +0200

    feat: add search service

---
+++ src/services/search.js
@@ -0,0 +1,9 @@
+import YoutubeApi, { baseParams } from "./config";
+
+export const search = (query) =>
+  YoutubeApi.get("/search", {
+    params: {
+      ...baseParams,
+      q: query,
+    },
+  });
