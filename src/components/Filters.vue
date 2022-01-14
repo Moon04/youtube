c5c11a7a72805482b63758314497501aee6a4727
moon04 <sydeny254@gmail.com>
2022-01-15 00:12:41 +0200

    feat: add filters section ui

---
+++ src/components/Filters.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="filters">
+    <select id="type" name="type">
+      <option value="all">All</option>
+      <option value="channel">Channel</option>
+      <option value="playlist">Playlist</option>
+    </select>
+    <select id="date" name="date">
+      <option value="all">Any time</option>
+      <option value="today">Today</option>
+      <option value="week">This week</option>
+      <option value="month">This month</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Filters",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/components/filters.scss";
+</style>
